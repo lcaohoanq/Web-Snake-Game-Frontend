a0fc736056fa4e3b51dde4a7d261fa4be97484d5
hoangclw@gmail.com <hoangclw@gmail.com>
2024-01-25 08:46:57 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. <br/>
+          Hello World! This is my first React App.
         </p>
         <a
           className="App-link"
