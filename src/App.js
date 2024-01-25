473afd971343d047523eada1a8bd317f8d0081e5
hoangclw@gmail.com <hoangclw@gmail.com>
2024-01-25 22:10:26 +0700

    refactor: change web theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. <br/>
-          Hello World! This is my first React App.
-        </p>
+        <h1>Snake Game</h1>
+      </header>
+      <section id="tmp">
+        <section id="App-img">
+          <img src="./logo.png" className="App-logo" alt="logo" />
+        </section>
+        <section id="App-data">
+          <div className="form-floating mb-3">
+            <input
+              type="username"
+              className="form-control"
+              id="floating"
+              placeholder="Username"
+            />
+            <label htmlFor="floatingInput">Username</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="floating"
+              placeholder="Password"
+            />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="floating"
+              placeholder="Confirm-Password"
+            />
+            <label htmlFor="floatingCPassword">Confirm-Password</label>
+          </div>
+          <div class="submit-btn">
+            <button type="submit">Submit</button>
+          </div>
+          <div class="bottom-btn">
+            <p>Do not have account?</p>
+            <button type="submit">Sign up here</button>
+          </div>
+        </section>
+      </section>
+      <footer>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/lcaohoanq/Java-Snake-Game"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Follow my Github for more, click here !
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
