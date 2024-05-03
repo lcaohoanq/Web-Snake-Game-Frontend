694750b67e8ad7f0f2f82a129251fe5bbfbd9080
lcaohoanq <hoangclw@gmail.com>
2024-05-03 11:13:32 +0700

    feat: migrate webpack, pug, typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// import scss
+import importAll from './util/importAll';
+
+//import scss
+importAll(require.context('./styles/', true, /scss/i));
+
+// import ts
+importAll(require.context('./components/', true, /ts/i));
+
+//import image
+importAll(require.context('./assets/images', false, /\.(gif|png|jpe?g|svg)$/i));
